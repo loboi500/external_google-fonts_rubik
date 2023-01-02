36cc5a60edf8b03753d90bec9798144056360fcb
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-02 17:14:19 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- fonts.mk
+++ fonts.mk
@@ -1,25 +1,3 @@
-# Copyright (C) 2018 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
-# PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
-# get installed too.
-
-PRODUCT_PACKAGES := \
-    Rubik-Regular.ttf \
-    Rubik-Italic.ttf \
-    Rubik-Medium.ttf \
-    Rubik-MediumItalic.ttf \
-    Rubik-Bold.ttf \
-    Rubik-BoldItalic.ttf
+version https://git-lfs.github.com/spec/v1
+oid sha256:51e03b586770f1e6d81b182ff76e57561c4b017e0dbce9b08d477e00f7b79b24
+size 942
